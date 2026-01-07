fbcb788c7d3cd9dc3d380170b8347ed22e44ebd1
Flavio <flavio.tomas.villa@gmail.com>
2026-01-07 13:36:04 -0300

    Começand o JS, criando uma variável para os botões de foco, intervalo e intervalo longo

---
+++ script.js
@@ -0,0 +1,3 @@
+const focusButton = document.querySelector('.js-focus-button')
+const breakButton = document.querySelector('.js-break-button')
+const longBreakButton = document.querySelector('.js-long-break-button')
